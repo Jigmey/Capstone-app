3a3c9f7dc92724a05441c5b7851f94c5f58511d0
Jigmey <jigmeydo@gmail.com>
2017-01-28 21:41:05 -0500

    Fixed routing issue, still need to fully fix post/comment.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,16 @@
 class CommentsController < ApplicationController
 
 	def create
+		@user = User.find(params[:id])
 		p "comment start"
-		@comment=Comment.create(text: params["body"][0], user_id: current_user.id, post_id: params[:post_id],is_this_profile: params[:is_this_profile], is_this_guild: params[:is_this_guild]  )
+		@comment=Comment.create(text: params["body"][0], user_id: params[:id], post_id: params[:post_id],is_this_profile: params[:is_this_profile], is_this_guild: params[:is_this_guild]  )
 		p "comment created"
-		if @comment=Comment.where(user_id: current_user.id).last.is_this_guild == "no" && @comment=Comment.where(user_id: current_user.id).last.is_this_profile == "no"
+		if @comment=Comment.where(user_id: @user.id).last.is_this_guild == "no" && @comment=Comment.where(user_id: @user.id).last.is_this_profile == "no"
 			redirect_to '/generals'
 		elsif @comment=Comment.where(user_id: @user.id).last.is_this_guild == "yes"
-			redirect_to "/guilds/#{current_user.guilds.first.id}"
+			redirect_to "/guilds/#{@user.guilds.first.id}"
 		elsif @comment=Comment.where(user_id: @user.id).last.is_this_profile == "yes"
-			redirect_to "/generals/#{current_user.id}"
+			redirect_to "/generals/#{@user.id}"
 		end
 	end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,20 @@
 class PostsController < ApplicationController
 
 	def create
+		@user = User.find(params[:id])
 		p "starting post"
-		@post=Post.new(text: params["body"][0], user_id: current_user.id, is_this_guild: params[:is_this_guild],is_this_profile: params[:is_this_profile], guild_id: current_user.guilds.ids.first )
+		@post=Post.new(text: params["body"][0], user_id: @user.id, is_this_guild: params[:is_this_guild],is_this_profile: params[:is_this_profile], guild_id: current_user.guilds.ids.first )
 		@post.save
 		p current_user.guilds.ids.first
 		p @post.errors.full_messages
 		p "post created?"
 		#need to add the hidden field tag so that it picks up the value and creates it specifically for the guild.
-		if @post=Post.where(user_id: current_user.id).last.is_this_guild == "no" && @post=Post.where(user_id: current_user.id).last.is_this_profile == "no"
+		if @post=Post.where(user_id: @user.id).last.is_this_guild == "no" && @post=Post.where(user_id: @user.id).last.is_this_profile == "no"
 			redirect_to '/generals'
-		elsif @post=Post.where(user_id: current_user.id).last.is_this_guild == "yes"
-			redirect_to "/guilds/#{current_user.guilds.first.id}"
-		elsif @post=Post.where(user_id: current_user.id).last.is_this_profile == "yes"
-			redirect_to "/generals/#{current_user.id}"
+		elsif @post=Post.where(user_id: @user.id).last.is_this_guild == "yes"
+			redirect_to "/guilds/#{@user.guilds.first.id}"
+		elsif @post=Post.where(user_id: @user.id).last.is_this_profile == "yes"
+			redirect_to "/generals/#{@user.id}"
 		end
 	end
 		
